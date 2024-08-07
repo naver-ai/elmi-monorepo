a45fbe025960acb41286c42a5e1b9c02091d5162
Young-Ho Kim <yghokim@gmail.com>
2024-08-07 22:55:18 +0900

    backend should always run 0.0.0.0

--- apps/backend/backend/run.py
+++ apps/backend/backend/run.py
@@ -3,7 +3,7 @@ from backend.utils.env_helper import get_env_variable, EnvironmentVariables
 
 if __name__ == "__main__":
     uvicorn.run("server:app", 
-                host=get_env_variable(EnvironmentVariables.BACKEND_HOSTNAME), 
+                host="0.0.0.0", 
                 port=int(get_env_variable(EnvironmentVariables.BACKEND_PORT)), 
                 reload=True)
 
