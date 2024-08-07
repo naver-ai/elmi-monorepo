74c2a45f01e42d419a0190f2c4bc1cb1a04ef66a
Young-Ho Kim <yghokim@gmail.com>
2024-08-07 10:46:41 +0900

    Adding a new project button

--- apps/elmi-web/src/features/projects/pages/ProjectsPage.tsx
+++ apps/elmi-web/src/features/projects/pages/ProjectsPage.tsx
@@ -6,6 +6,9 @@ import { Card, Image, Space, Spin } from "antd"
 import { useNavigate } from "react-router-dom"
 import { useNetworkImageSource } from "../hooks"
 import { Http } from "../../../net/http"
+import { PlusIcon } from "@heroicons/react/24/solid"
+
+const CARD_CLASSNAME = "transition hover:shadow-lg hover:scale-105 cursor-pointer"
 
 const ProjectCard = (props: {id: string}) => {
     const project = useSelector(state => projectsEntitySelectors.selectById(state, props.id))
@@ -32,10 +35,16 @@ const ProjectCard = (props: {id: string}) => {
         nav(`/app/projects/${props.id}`)
     }, [nav, props.id])
 
-    return <Card bordered={false} size="default" className="transition hover:shadow-lg hover:scale-105 cursor-pointer" 
+    return <Card bordered={false} size="default" className={CARD_CLASSNAME} 
         loading={project == null} onClick={onEnter} cover={<Image src={coverImageSource || undefined} preview={false}/>}>
         <Card.Meta title={project?.song_title} description={<span className="font-bold">{project?.song_artist}</span>}/>
         <Card.Meta className="text-xs pt-2" description={accessMessage}/>
+    </Card>
+}
+
+const NewProjectButton = () => {
+    return <Card bordered={false} size="default" className={CARD_CLASSNAME} cover={<div className="aspect-square bg-slate-100 rounded-t-lg !flex justify-center items-center"><PlusIcon className="w-10 h-10 text-gray-400"/></div>}>
+        <Card.Meta title={<span className="text-center">Add new project</span>}/>
     </Card>
 }
 
@@ -57,6 +66,7 @@ export const ProjectsPage = () => {
             {
                 projectIds.map(id => <ProjectCard key={id} id={id}/>)
             }
+            <NewProjectButton/>
         </div>
         }
     </SignedInScreenFrame>
